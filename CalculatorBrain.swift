7bd6fd40920d62820f35db46c6ba0f8fbd4b778a
manoelfilho <manoel.tsf@gmail.com>
2021-01-28 14:46:32 -0300

    Acrescimo das imagens de preview e correcao dos termo de alerta

--- CalculatorBrain.swift
+++ CalculatorBrain.swift
@@ -17,11 +17,11 @@ struct CalculatorBrain {
         let bmiValue = weight / (height * height)
         
         if bmiValue < 18.5 {
-            bmi = BMI(value: bmiValue, advice: "Coma mais bolo!", color: #colorLiteral(red: 0.4745098054, green: 0.8392156959, blue: 0.9764705896, alpha: 1))
+            bmi = BMI(value: bmiValue, advice: "Abaixo do peso!", color: #colorLiteral(red: 0.4745098054, green: 0.8392156959, blue: 0.9764705896, alpha: 1))
         }else if bmiValue < 24.9 {
-            bmi = BMI(value: bmiValue, advice: "Fica de boa!", color: #colorLiteral(red: 0.4666666687, green: 0.7647058964, blue: 0.2666666806, alpha: 1))
+            bmi = BMI(value: bmiValue, advice: "Peso ideal!", color: #colorLiteral(red: 0.4666666687, green: 0.7647058964, blue: 0.2666666806, alpha: 1))
         }else{
-            bmi = BMI(value: bmiValue, advice: "Coma menos bolo!", color: #colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1))
+            bmi = BMI(value: bmiValue, advice: "Acima do peso!", color: #colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1))
         }
         
     }
